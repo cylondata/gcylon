9a3a5d6ff300d863fd837b4e20fb1fc41958bceb
Ahmet Uyar <ahmetuyar@gmail.com>
2021-04-09 17:33:38 +0300

    improved conda build with cpp

--- conda/recipes/gcylon/build.sh
+++ conda/recipes/gcylon/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # export CYLON_HOME=/path/to/cylon_home
-# export GCYLON_HOME=/path/to/gcylon_home
 # export CUDACXX=/path/to/nvcc
 # export CC=/path/to/gcc
 # export CXX=/path/to/c++
 
-./build.sh --conda_cpp --build_path ${pwd}/cbuild
-#./build.sh --conda_cpp --install_path ${BUILD_PREFIX}
+./build.sh --conda_cpp --install_path ${BUILD_PREFIX}
+# ./build.sh --conda_cpp --install_path ${BUILD_PREFIX}/cbuild --build_path ${BUILD_PREFIX}/cbuild
+# ./build.sh --conda_cpp
