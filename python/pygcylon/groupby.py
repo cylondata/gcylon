cc89bccdc0fa1dd4e95f8339ac8e6225f8c269d5
Ahmet Uyar <ahmetuyar@gmail.com>
2021-05-21 15:13:25 +0300

    implemented single shuffling per groupby object

--- python/pygcylon/groupby.py
+++ python/pygcylon/groupby.py
@@ -28,6 +28,132 @@ class GroupByDataFrame(object):
     def __init__(
         self, df: pygcylon.DataFrame, by=None, level=None, sort=False, as_index=True, dropna=True, env: CylonEnv = None
     ):
+        """
+        Group DataFrame using a mapper or by a Series of columns.
+        Works with both single DataFrame or distributed DataFrame
+
+        A groupby operation involves some combination of splitting the object,
+        applying a function, and combining the results. This can be used to
+        group large amounts of data and compute operations on these groups.
+
+        When calculating groupby in distributed DataFrames,
+        an all-to-all shuffle communication operation is performed.
+        It is very important to avoid unnecessary shuffle operations,
+        since shuffling the data among distributed workers are constly.
+
+        When a GroupByDataFrame object is created, and the first groupby operation is performed,
+        this shuffle operation is performed by partitioning the tables on the groupby columns and
+        all datarrame columns are shuffled.
+
+        So, to get the best performance in a distributed dataframe,
+        one should first create a GroupByDataFrame object and perform many aggregations on it.
+        Because, creating and performing a groupby object requires a distributed shuffle.
+        When we reuse the same GroupByDataFrame, we avoid re-shuffling the dataframe
+        For example following code performs a single shuffle only:
+            gby = df.groupby(["column1", "column2"], ..., env=env)
+            gby.sum()
+            gby["columnx"].mean()
+            gby[["columnx", "columny"]].min()
+
+        One must avoid always running the groupby on the dataframe object.
+        For example, all three of the following operations perform the distributed shuffle
+            df.groupby("columnq", env=env)["columnb"].sum()
+            df.groupby("columnq", env=env)["columnb"].mean()
+            df.groupby("columnq", env=env)["columnc"].mean()
+        One can easily perform a single shuffle for these three lines by first creating a GroupByDataFrame object
+        and performing the aggragations using it
+
+        A second important point is to create a new dataframe from a subset of columns
+        and performing the groupby on it when working with dataframe with many columns.
+        Suppose, you are working with a dataframe with hundreds of columns
+        but you would like to perform the groupby and aggregations on a small number of columns.
+        First, you need to create a new dataframe with those groupby and aggregations columns.
+        Then, perform the groupby on this new dataframe.
+        This will avaoid shufling the whole dataframe. Only the columns on the new dataframe will be shuffled.
+            df2 = df[["columnx", "columny", "columnz", ...]]
+            gby = df2.groupby("columnx", env=env)
+            gby["columny"].sum()
+            gby["columnz"].mean()
+
+
+        Parameters
+        ----------
+        by : mapping, function, label, or list of labels
+            Used to determine the groups for the groupby. If by is a
+            function, it’s called on each value of the object’s index.
+            If a dict or Series is passed, the Series or dict VALUES will
+            be used to determine the groups (the Series’ values are first
+            aligned; see .align() method). If a cupy array is passed, the
+            values are used as-is determine the groups. A label or list
+            of labels may be passed to group by the columns in self.
+            Notice that a tuple is interpreted as a (single) key.
+        level : int, level name, or sequence of such, default None
+            If the axis is a MultiIndex (hierarchical), group by a particular
+            level or levels.
+        as_index : bool, default True
+            For aggregated output, return object with group labels as
+            the index. Only relevant for DataFrame input.
+            as_index=False is effectively “SQL-style” grouped output.
+        sort : bool, default False
+            Sort result by group key. Differ from Pandas, cudf defaults to
+            ``False`` for better performance. Note this does not influence
+            the order of observations within each group. Groupby preserves
+            the order of rows within each group.
+        dropna : bool, optional
+            If True (default), do not include the "null" group.
+        env: CylonEnv needs to be provided for distributed groupby operation.
+
+        Returns
+        -------
+            DataFrameGroupBy
+                Returns a groupby object that contains information
+                about the groups.
+
+        Examples
+        --------
+        >>> import pygcylon as gc
+        >>> # first try local groupby on a single DataFrame
+        >>> env: gc.CylonEnv = gc.CylonEnv(config=gc.MPIConfig(), distributed=True)
+        >>> df = gc.DataFrame({'a': [1, 1, 1, 2, 2], 'b': [1, 1, 2, 2, 3], 'c': [1, 2, 3, 4, 5]})
+        >>> df
+           a  b  c
+        0  1  1  1
+        1  1  1  2
+        2  1  2  3
+        3  2  2  4
+        4  2  3  5
+        >>> # create a groupby object and perform multiple operations
+        >>> gby = df.groupby("a")
+        >>> gby.sum()
+           b  c
+        a
+        2  5  9
+        1  4  6
+        >>> gby.max()
+           b  c
+        a
+        2  3  5
+        1  2  3
+        >>> gby["b"].sum()
+           b
+        a
+        2  5
+        1  4
+        >>> # to perform groupby on a different set of columns, we need to create a new GroupByDataFrame object
+        >>> gby = df.groupby(["a", "b"])
+        >>> gby.sum()
+             c
+        a b
+        1 2  3
+        2 2  4
+          3  5
+        1 1  3
+
+        >>> # todo: add distributed DataFrame examples
+        >>> env: gc.CylonEnv = gc.CylonEnv(config=gc.MPIConfig(), distributed=True)
+
+        """
+
         self._df = df
         self._by = by
         self._level = level
@@ -47,14 +173,13 @@ class GroupByDataFrame(object):
         self._shuffled_cdf = None
         self._cudf_groupby = None
 
-    def _shuffle(self, cdf: cudf.DataFrame = None):
+    def _shuffle(self):
         """
         Shuffle the dataframe if there are multiple workers in the job
         after shuffling, construct the cudf groupby object
         if cdf is None, use the original cudf dataframe
         """
-        if cdf is None:
-            cdf = self._df.to_cudf()
+        cdf = self._df.to_cudf()
 
         if self._env is None or self._env.world_size == 1:
             self._shuffled_cdf = cdf
@@ -90,9 +215,11 @@ class GroupByDataFrame(object):
         if not self._shuffled_cdf:
             self._shuffle()
 
-        result = None
         if self._value_columns:
-            result = self._cudf_groupby[self._value_columns].__getattribute__(key)
+            try:
+                result = self._cudf_groupby[self._value_columns].__getattribute__(key)
+            finally:
+                self._value_columns = []
         else:
             result = self._cudf_groupby.__getattribute__(key)
 
@@ -118,14 +245,11 @@ class GroupByDataFrame(object):
         """
         when a column name or names are given with the subscript operator,
         select those columns as groupby value columns
+        Given column names are saved to e used in the next call of __getattribute__ method
+        when the next groupby operation is performed After the call of __getattribute__ method,
+        these column names are deleted.
 
-        if these columns are a subset of the original cudf dataframe,
-        create a new dataframe for this subset and perform the shuffle operation on them
-
-        if the shuffle is already performed on those columns in the previous selection,
-        there is no need to apply again
-        if the shuffle is performed on a superset of columns in the previous selection,
-        also there is no need to apply again
+        We assume that after this method, an aggregation function will always be called.
         """
 
         self._value_columns = []
@@ -146,26 +270,4 @@ class GroupByDataFrame(object):
         else:
             raise ValueError("Please provide (int, str, tuple) or list of these as column names: ", key)
 
-        # calculate columns of groupby dataframe,
-        # if _level, it is already value_columns,
-        # otherwise, it is
-        #   groupby columns + value columns
-        all_gby_columns = self._value_columns
-        if not self._level:
-            all_gby_columns = self._grouping_columns + self._value_columns
-
-        # if value_columns are equal to the shuffled_cdf columns, do nothing
-        if self._shuffled_cdf and _subset_of(all_gby_columns, self._shuffled_cdf.columns.to_list()):
-            return self
-
-        # if the value_columns are equal to all cdf columns, no need to create a subset df
-        # shuffle on all columns
-        if _two_lists_equal(all_gby_columns, column_names):
-            self._shuffle()
-            return self
-
-        # create a new dataframe as a subset of columns
-        # perform the shuffle on this new cdf
-        subset_cdf = self._df.to_cudf()[all_gby_columns]
-        self._shuffle(subset_cdf)
         return self
